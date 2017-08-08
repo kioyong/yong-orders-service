504808d3e79d8ceecf8f4e1b7045b59b94910052
LiangYong <yong.a.liang@accenture.com>
2017-08-08 17:00:31 +0800

    impl user initializer

--- src/main/java/com/yong/orders/controller/IndexController.java
+++ src/main/java/com/yong/orders/controller/IndexController.java
@@ -2,7 +2,7 @@ package com.yong.orders.controller;
 
 import com.yong.orders.service.UserService;
 import com.yong.orders.model.User;
-import lombok.extern.slf4j.Slf4j;
+//import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@Slf4j
+//@Slf4j
 public class IndexController {
 
     @Autowired
